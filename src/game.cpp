626aeb2a8284d4b81bb3386c211c1554041c0aab
Oskar Wróbel <o.wrobel2025@protonmail.com>
2025-10-25 18:10:09 +0200

    Cleanup. No more unnecessary namespaces

--- src/game.cpp
+++ src/game.cpp
@@ -1,8 +1,10 @@
 #include "game.h"
 
+#include <raylib.h>
 #include <string>
 #include <algorithm>
 #include <cmath>
+#include <vector>
 
 namespace Game {
 
@@ -28,7 +30,6 @@ Input Input::Capture()
 }
 
 //LEVEL
-namespace Level {
 
     Grid::Grid(size_t width, size_t height) :
         size_x(width),
@@ -51,7 +52,7 @@ namespace Level {
 
 
     Grid Grid::NewDefault(unsigned int width, unsigned int height){
-        Level::Grid grid(width, height);
+        Grid grid(width, height);
         for (int y = 0; y < height; y++)
         {
             grid.Place(0, y, 6);
@@ -73,33 +74,19 @@ namespace Level {
     }
 
 
-    void Render(const Level::Grid& grid, const std::vector<Texture2D>& tile_textures){
-        for (unsigned int y = 0; y < grid.size_y; y++)
-        {
-            for (unsigned int x = 0; x < grid.size_x; x++)
-            {
-                const Texture2D tile_texture = tile_textures.at(grid.GetTile(x, y).type);
-
-                DrawTexture(tile_texture, x * Config::TILE_RESOLUTION, y * Config::TILE_RESOLUTION, WHITE);
-            }
-        }
-    }
-}
-
 // PLAYER
-//
-namespace Player {
-    void State::MoveBy(Vector2 offset) {
+
+    void PlayerState::MoveBy(Vector2 offset) {
         Vector2 new_position = {position.x + offset.x, position.y + offset.y};
         position.x += offset.x;
         position.y += offset.y;
     }
 
-    Vector2 State::GetCenter() const {
+    Vector2 PlayerState::GetCenter() const {
         return {position.x + size.x / 2, position.y + size.y / 2};
     }
 
-    State State::New(
+    PlayerState PlayerState::New(
         Vector2 position,
         Vector2 size,
         float move_speed
@@ -108,7 +95,7 @@ namespace Player {
         return {position, size, move_speed};
     }
 
-    void State::Update(
+    void PlayerState::Update(
         const Input& input,
         float delta_time)
     {
@@ -125,61 +112,51 @@ namespace Player {
 
     }
 
-    void Render(const State& state, const Texture2D& texture){
-        DrawTextureV(texture, state.position, WHITE);
-    }
-}
 
 //CAMERA
-namespace Camera{
-    Camera2D State::ToCamera2D() const {
-        return {offset, target, rotation, zoom};
-    }
+Camera2D CameraState::ToCamera2D() const {
+    return {offset, target, rotation, zoom};
+}
 
-    void State::UpdateZoom(float mouse_wheel_input, bool ctrl_held){
-        if (mouse_wheel_input != 0 && ctrl_held){
-            // Get the world point that is under the mouse
-            Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), ToCamera2D());
+void CameraState::UpdateZoom(float mouse_wheel_input, bool ctrl_held){
+    if (mouse_wheel_input != 0 && ctrl_held){
+        // Get the world point that is under the mouse
+        Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), ToCamera2D());
 
-            if (Config::ZOOM_INTO_MOUSE){
-                // Set the offset to where the mouse is
-                offset = GetMousePosition();
+        if (Config::ZOOM_INTO_MOUSE){
+            // Set the offset to where the mouse is
+            offset = GetMousePosition();
 
-                // Set the target to match, so that the camera maps the world space point
-                // under the cursor to the screen space point under the cursor at any zoom
-                target = mouseWorldPos;
-
-            }
-
-            // Zoom increment
-            // Uses log scaling to provide consistent zoom speed
-            float scale = 0.2f * mouse_wheel_input;
-            zoom = std::clamp(std::expf(std::logf(zoom) + scale), 1 / 8.f, 64.0f);
+            // Set the target to match, so that the camera maps the world space point
+            // under the cursor to the screen space point under the cursor at any zoom
+            target = mouseWorldPos;
 
         }
-    }
 
-    void State::UpdatePosition(Vector2 player_center, float window_width, float window_height){
-        target = player_center;
-        offset = {window_width / 2, window_height / 2};
-    }
+        // Zoom increment
+        // Uses log scaling to provide consistent zoom speed
+        float scale = 0.2f * mouse_wheel_input;
+        zoom = std::clamp(std::expf(std::logf(zoom) + scale), 1 / 8.f, 64.0f);
 
-    void State::Update(Vector2 player_center, Input input, float window_width, float window_height){
-        UpdatePosition(player_center, window_width, window_height);
-        UpdateZoom(input.mouse_wheel, input.held.ctrl);
     }
 }
 
-//GLOBALS
-Game::GameState state{};
-Game::Assets assets{};
+void CameraState::UpdatePosition(Vector2 player_center, float window_width, float window_height){
+    target = player_center;
+    offset = {window_width / 2, window_height / 2};
+}
+
+void CameraState::Update(Vector2 player_center, Input input, float window_width, float window_height){
+    UpdatePosition(player_center, window_width, window_height);
+    UpdateZoom(input.mouse_wheel, input.held.ctrl);
+}
 
 //FUNCTIONS
-Vector2 GetMouseGridPosition(Vector2 mouse_position, const Game::Camera::State& camera, unsigned int resolution){
+Vector2 GetMouseGridPosition(Vector2 mouse_position, const CameraState& camera, unsigned int tile_resolution){
     Vector2 world_position = GetScreenToWorld2D(mouse_position, camera.ToCamera2D());
     return {
-        std::floor(world_position.x / Game::Config::TILE_RESOLUTION),
-        std::floor(world_position.y / Game::Config::TILE_RESOLUTION),
+        std::floor(world_position.x / tile_resolution),
+        std::floor(world_position.y / tile_resolution),
     };
 
 }
@@ -210,22 +187,27 @@ void Init(
     std::string name,
     unsigned int window_width,
     unsigned int window_height,
-    unsigned int framerate,
-    unsigned int tile_resolution,
-    const unsigned int tile_type_count
+    unsigned int framerate
 ){
-    InitWindow(Config::WINDOW_WIDTH, Config::WINDOW_HEIGHT, "raylib basic window");
+    InitWindow(window_width, window_height, name.c_str());
     if (framerate != 0){
         SetTargetFPS(framerate);
 
     }
+}
+
+Assets InitAssets(unsigned int tile_resolution, unsigned int tile_type_count){
+    Assets assets;
 
     assets.tile_spritesheet = LoadImage("assets/tiles.png");
     assets.tile_textures = GetTileTextures(assets.tile_spritesheet, tile_resolution, tile_type_count);
-
     assets.player_texture = LoadTexture("assets/player.png");
 
+    return assets;
+
 }
+
+//UPDATE
 
 void UpdateTilePlacing(GameState& state){
     if (state.input.pressed.space){
@@ -265,24 +247,50 @@ void Update(GameState& state){
 
 }
 
-void RenderTilePreview(unsigned int tile_type, Vector2 position){
-    Texture2D texture = assets.tile_textures.at(tile_type);
+//RENDER
+
+void RenderGrid(const Grid& grid, const std::vector<Texture2D>& tile_textures, unsigned int tile_resolution){
+    for (unsigned int y = 0; y < grid.size_y; y++)
+    {
+        for (unsigned int x = 0; x < grid.size_x; x++)
+        {
+            const Texture2D& tile_texture = tile_textures.at(grid.GetTile(x, y).type);
+
+            DrawTexture(tile_texture, x * tile_resolution, y * tile_resolution, WHITE);
+        }
+    }
+
+}
+
+void RenderTilePreview(unsigned int tile_type, Vector2 position, const std::vector<Texture2D>& tile_textures){
+    Texture2D texture = tile_textures.at(tile_type);
     DrawTextureEx(texture, position, 0, 6, {255, 255, 255, 100});
 
 }
 
-void RenderTileGhost(unsigned int tile_type, Vector2 position){
-    Texture2D texture = assets.tile_textures.at(tile_type);
+void RenderTileGhost(
+    unsigned int tile_type,
+    Vector2 position,
+    const std::vector<Texture2D>& tile_textures,
+    unsigned int tile_resolution
+)
+{
+    Texture2D texture = tile_textures.at(tile_type);
 
     Rectangle rectangle{
-        position.x * Config::TILE_RESOLUTION,
-        position.y * Config::TILE_RESOLUTION,
-        Config::TILE_RESOLUTION,
-        Config::TILE_RESOLUTION
+        position.x * tile_resolution,
+        position.y * tile_resolution,
+        static_cast<float>(tile_resolution),
+        static_cast<float>(tile_resolution)
     };
     DrawRectangleRec(rectangle, {0, 0, 0, 130});
     DrawTextureV(texture, {rectangle.x, rectangle.y}, {255, 255, 255, 130});
     DrawRectangleLinesEx(rectangle, 1, {255, 255, 255, 130});
+
+}
+
+void RenderPlayer(const PlayerState& state, const Texture2D& texture){
+    DrawTextureV(texture, state.position, WHITE);
 
 }
 
@@ -293,15 +301,20 @@ void Render(const GameState& state, const Assets& assets){
     //START DRAWING
     BeginMode2D(state.camera.ToCamera2D());
 
-    Level::Render(state.grid, assets.tile_textures);
-    RenderTileGhost(state.tile_place_type, GetMouseGridPosition(state.input.mouse_position, state.camera));
+    RenderGrid(state.grid,  assets.tile_textures, Config::TILE_RESOLUTION);
+    RenderTileGhost(
+        state.tile_place_type,
+        GetMouseGridPosition(state.input.mouse_position, state.camera),
+        assets.tile_textures,
+        Config::TILE_RESOLUTION
+    );
 
-    Player::Render(state.player , assets.player_texture);
+    RenderPlayer(state.player , assets.player_texture);
 
     EndMode2D();
 
     //Draw UI
-    RenderTilePreview(state.tile_place_type, {Config::WINDOW_WIDTH - 80, 30});
+    RenderTilePreview(state.tile_place_type, {Config::WINDOW_WIDTH - 80, 30}, assets.tile_textures);
     DrawText("It works!", 32, 32, 32, WHITE);
     DrawFPS(60, 60);
 
@@ -310,12 +323,16 @@ void Render(const GameState& state, const Assets& assets){
 }
 
 void Run(){
-    Game::Init("CaveSlave");
+    Init("CaveSlave"); //Has to be first to be called
+
+    auto assets = InitAssets();
+    GameState state{};
 
     while (!WindowShouldClose()){
         Update(state);
         Render(state, assets);
     }
+
     CloseWindow();
 
 }
--- src/game.h
+++ src/game.h
@@ -22,8 +22,6 @@ struct Config {
     static constexpr unsigned int TILE_SIZE = TILE_RESOLUTION * TILE_COUNT;
 };
 
-
-
 struct Input {
     struct Pressed{
         bool space;
@@ -44,77 +42,61 @@ struct Input {
 
 };
 
-namespace Level {
-
-    struct Tile {
-        unsigned int type;
-    };
-
-    struct Grid{
-        const unsigned int size_x;
-        const unsigned int size_y;
-
-        std::vector<std::vector<Tile>> tiles;
-
-        Grid(size_t width, size_t height);
-
-        void Place(unsigned int x, unsigned int y, unsigned int type);
-
-        Tile GetTile(unsigned int x, unsigned int y) const; //TODO: Maybe make this a reference idk
-
-
-        static Grid NewDefault(unsigned int width = Config::GRID_SIZE_X, unsigned int height = Config::GRID_SIZE_Y);
-    };
-
-    void Render(const Level::Grid& grid, const std::vector<Texture2D>& tile_textures);
-
+struct Tile {
+    unsigned int type;
 };
 
-namespace Player {
-    struct State {
-        Vector2 position;
-        Vector2 size;
-        float move_speed;
+struct Grid{
+    const unsigned int size_x;
+    const unsigned int size_y;
 
-        void MoveBy(Vector2 offset);
+    std::vector<std::vector<Tile>> tiles;
 
-        Vector2 GetCenter() const;
-        static State New(
-            Vector2 position = {0., 0.},
-            Vector2 size = {8.0f, 8.0f},
-            float move_speed = 400.0f
-        );
+    Grid(size_t width, size_t height);
 
-        void Update(const Input& input, float delta_time);
-    };
+    void Place(unsigned int x, unsigned int y, unsigned int type);
 
-    void Render(const State& state, const Texture2D& texture);
+    Tile GetTile(unsigned int x, unsigned int y) const; //TODO: Maybe make this a reference idk
 
-}
+    static Grid NewDefault(unsigned int width = Config::GRID_SIZE_X, unsigned int height = Config::GRID_SIZE_Y);
+};
+
+struct PlayerState {
+    Vector2 position;
+    Vector2 size;
+    float move_speed;
+
+    void MoveBy(Vector2 offset);
+
+    Vector2 GetCenter() const;
+    static PlayerState New(
+        Vector2 position = {0., 0.},
+        Vector2 size = {8.0f, 8.0f},
+        float move_speed = 400.0f
+    );
+
+    void Update(const Input& input, float delta_time);
+};
 
 //CAMERA
+struct CameraState {
+    Vector2 offset = {0., 0.};
+    Vector2 target = {0., 0.};
+    float rotation = 0.;
+    float zoom = 2.;
 
-namespace Camera {
-    struct State {
-        Vector2 offset = {0., 0.};
-        Vector2 target = {0., 0.};
-        float rotation = 0.;
-        float zoom = 2.;
+    Camera2D ToCamera2D() const;
 
-        Camera2D ToCamera2D() const;
+    void UpdateZoom(float mouse_wheel_input, bool ctrl_held);
 
-        void UpdateZoom(float mouse_wheel_input, bool ctrl_held);
+    void UpdatePosition(Vector2 player_center, float window_width, float window_height);
 
-        void UpdatePosition(Vector2 player_center, float window_width, float window_height);
-
-        void Update(Vector2 player_center,
-            Input input,
-            float window_width = Config::WINDOW_WIDTH,
-            float window_height = Config::WINDOW_HEIGHT
-        );
-    };
-
-}
+    void Update(Vector2 player_center,
+        Input input,
+        float window_width = Config::WINDOW_WIDTH,
+        float window_height = Config::WINDOW_HEIGHT
+    );
+};
 
 struct Assets{
     Image tile_spritesheet;
@@ -124,20 +106,18 @@ struct Assets{
 
 };
 
-
 struct GameState{
     float delta_time;
     Input input;
     unsigned int tile_place_type = 1;
-    Level::Grid grid = Level::Grid::NewDefault();
-    Player::State player = Player::State::New({0, 0});
-    Camera::State camera;
+    Grid grid = Grid::NewDefault();
+    PlayerState player = PlayerState::New({0, 0});
+    CameraState camera;
 
 }; //TODO: Remove unnecessary passing of state instead of its variables
 
 //FUNCTIONS
-
-Vector2 GetMouseGridPosition(Vector2 mouse_position, const Camera::State& camera, unsigned int resolution = Config::TILE_RESOLUTION);
+Vector2 GetMouseGridPosition(Vector2 mouse_position, const CameraState& camera, unsigned int resolution = Config::TILE_RESOLUTION);
 
 
 std::vector<Texture2D> GetTileTextures(const Image &spritesheet, unsigned int tile_resolution, unsigned int tile_type_count);
@@ -146,18 +126,30 @@ void Init(
     std::string name,
     unsigned int window_width = Config::WINDOW_WIDTH,
     unsigned int window_height = Config::WINDOW_HEIGHT,
-    unsigned int framerate = Config::TARGET_FRAMERATE,
+    unsigned int framerate = Config::TARGET_FRAMERATE
+);
+
+Assets InitAssets(
     unsigned int tile_resolution = Config::TILE_RESOLUTION,
-    const unsigned int tile_type_count = Config::TILE_COUNT
+    unsigned int tile_type_count = Config::TILE_COUNT
 );
 
 void UpdateTilePlacing(GameState& state);
 
 void Update(GameState& state);
 
-void RenderTilePreview(unsigned int tile_type, Vector2 position);
+void RenderGrid(const GameState& state, const Assets& assets, unsigned int tile_resolution);
 
-void RenderTileGhost(unsigned int tile_type, Vector2 position);
+void RenderTilePreview(unsigned int tile_type, Vector2 position, std::vector<Texture2D>& tile_textures);
+
+void RenderTileGhost(
+    unsigned int tile_type,
+    Vector2 position,
+    const std::vector<Texture2D>& tile_textures,
+    const unsigned int tile_resolution
+);
+
+void RenderPlayer(const PlayerState& player, const Texture2D& texture);
 
 void Render(const GameState& state, const Assets& assets);
 
